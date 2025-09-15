2cb9b3d549c05474230ec23e742baa8e52942552
Noel Odero <anoel.odero@gmail.com>
2025-09-15 07:06:27 +0200

    line graph

--- math/Learning/line.py
+++ math/Learning/line.py
@@ -1,11 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use('fivethirtyeight')
 years = [2006 + x for x in range(16)]
 
 weights = [50, 78, 83, 84, 85, 87, 67, 80, 82, 79, 76, 58, 75, 78, 87, 77]
 
-plt.plot(years, weights)
+plt.plot(years, weights, color="#444444", linestyle='--', marker='.')
+
+weights_2 = [51, 98, 83, 84, 85, 87, 87, 80, 100, 79, 76, 58, 75, 78, 87, 77]
+plt.plot(years, weights_2, "b", marker='o', lw=3)
+
+plt.legend(['first weight', 'second weight'])
+# plt.tight_layout()
+plt.grid()
 plt.xlabel("Year")
 plt.ylabel("Weight")
 plt.title("Weight Progression Over Years")
